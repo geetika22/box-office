4dee05580c82fcae0d57f61a0a7d4e5e0169fdd2
Geetika Agarwal <68004582+geetika2106@users.noreply.github.com>
2021-01-19 19:29:20 +0530

    replaced useState with useReducer on the show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,34 +1,57 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useReducer} from 'react'
 import {useParams} from 'react-router-dom';
 import {apiGet} from '../misc/config';
+
+
+const reducer = (previousState, action) => {
+    switch(action.type){
+
+        case 'FETCH_SUCCESS': {
+            return  {isLoading: false, show: action.show, error: null}
+        }
+
+        case 'FETCH_FAILED': {
+            return {...previousState, isLoading: false, error: action.error}
+        }
+
+        default : return previousState
+    }
+}
+
+const initialState = {
+    show: null,
+    isLoading: true,
+    error: null,
+}
 
 const Show = () =>{
 
     const { id } = useParams()
 
-    const [show, setShow] = useState(null)
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [{show, isLoading, error},dispatch] = useReducer(reducer, initialState)
+
+    // eslint-disable-next-line
+    console.log(show);
 
     useEffect(() => {
 
         const isMounted = true
-        // eslint-disable-next-line 
-        console.log(show);
+        //  eslint-disable-next-line 
+        // console.log(show);
 
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
             if(isMounted){
-            setShow(results);
-            setIsLoading(false);
+
+                dispatch({ type: 'FETCH_SUCCESS', show: results})
+
             }
         }).catch(err => {
             if(isMounted) {
-            setError(err.message);
-            setIsLoading(false);
+                dispatch({ type: 'FETCH_FAILED', error: err.message})
             }
         })
 
-    }, [])
+    }, [id]);
 
     if(isLoading) {
         return <div>Data is being Loaded</div>
@@ -44,4 +67,4 @@ const Show = () =>{
         </div>
     ) }
 
-export default Show
+export default Show;
