a523da4107d4d2adcc776d5d606292b1e0fa6153
Geetika Agarwal <68004582+geetika2106@users.noreply.github.com>
2021-01-20 11:23:10 +0530

    defined theme for styled components and created a few styled components

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import {useLocation} from 'react-router-dom';
+import {NavList, LinkStyled} from './Navs.styled';
 
 const LINKS = [{to: '/', text: 'Home'},
                 {to : '/starred', text:'Starred'},
             ]
 
-const Navs = () => (
+const Navs = () => {
+const location = useLocation()
+return (
         <div>
-            <ul>
+            <NavList>
                 {
-                    LINKS.map(items =><li key = {items.to}><Link to={items.to}>{items.text}</Link></li>)
+                    LINKS.map(items =><li key = {items.to}><LinkStyled to={items.to} className={items.to === location.pathname ? 'active' : ''}>{items.text}</LinkStyled></li>)
                 }
-            </ul>
+            </NavList>
         </div>
     )
-
+}
 export default Navs
